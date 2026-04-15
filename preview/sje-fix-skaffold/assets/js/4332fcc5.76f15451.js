b1a1caab5559e623b41db4a68633762aa2c28443
mrsimonemms <mrsimonemms@users.noreply.github.com>
2026-04-15 08:51:27 +0000

    deploy: f3fa21a2bc355e62f4f96f6d1eac526c8efe05de

---
+++ preview/sje-fix-skaffold/assets/js/4332fcc5.76f15451.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunkzigflow=globalThis.webpackChunkzigflow||[]).push([[886],{18132(e){e.exports=JSON.parse('{"metadata":{"permalink":"/preview/sje-fix-skaffold/articles","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
---
+++ preview/sje-fix-skaffold/assets/js/acecf23e.093888e9.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunkzigflow=globalThis.webpackChunkzigflow||[]).push([[1903],{1912(s){s.exports=JSON.parse('{"blogBasePath":"/preview/sje-fix-skaffold/articles","blogTitle":"Blog","authorsListPath":"/preview/sje-fix-skaffold/articles/authors"}')}}]);
