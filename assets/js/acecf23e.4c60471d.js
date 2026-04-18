a08d50bb0dc3598d8c58cf9dbdbcb67aae621841
mrsimonemms <mrsimonemms@users.noreply.github.com>
2026-04-18 19:28:24 +0000

    deploy: b129e34808b41573de1f715bd51c3d1c056c7293

---
+++ assets/js/acecf23e.4c60471d.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkzigflow=self.webpackChunkzigflow||[]).push([["3220"],{1912(s){s.exports=JSON.parse('{"blogBasePath":"/articles","blogTitle":"Blog","authorsListPath":"/articles/authors"}')}}]);
