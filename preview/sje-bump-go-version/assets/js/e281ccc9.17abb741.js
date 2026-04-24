cf931df54c8d9e18c6a8b721f3469b7e69166bb3
mrsimonemms <mrsimonemms@users.noreply.github.com>
2026-04-24 08:13:41 +0000

    deploy: 1ab13a49911706f1bf6373814eee4541678f8bcb

---
+++ preview/sje-bump-go-version/assets/js/e281ccc9.17abb741.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkzigflow=self.webpackChunkzigflow||[]).push([["3430"],{14014(e){e.exports=JSON.parse('{"metadata":{"permalink":"/preview/sje-bump-go-version/articles","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
